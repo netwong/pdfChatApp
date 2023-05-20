f57ce90238124bbe77881ad1d9d6049ad64a7147
cksir <netwong@gmail.com>
2023-05-20 21:52:41 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+#Sidebar contents
+with st.sidebar:
+    st.title('LLM Chat App')
+    st.markdown('''
+    ''')
+    add_vertical_space(5)
+    st.write('Made with by [netcksir] (https://youtube.com/@netwong)')
